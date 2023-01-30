1763c51bd15d18ffca0279f535830ada3ede0b62
森本晃輔 <8tako8tako8@gmail.com>
2023-01-31 02:00:56 +0900

    mainでパッケージ間の疎通を確認

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/8tako8tako8/go_todo/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
