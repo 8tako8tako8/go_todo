c96a3de7e97ffe73ccaafc3c1f869def3d75c9b5
森本晃輔 <8tako8tako8@gmail.com>
2023-01-31 12:15:10 +0900

    configのinitにログ設定を追加

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 
+	"github.com/8tako8tako8/go_todo/utils"
 	"gopkg.in/go-ini/ini.v1"
 )
 
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
